08a50c2353bad6a3b522c704a70e20db3d69195b
masroor07 <76279044+masroor07@users.noreply.github.com>
2022-08-07 11:45:18 +0530

    Added dockerfile and Jenkinsfile for CI/CD

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,10 +21,8 @@ export default function TextForm(props) {
    navigator.clipboard.writeText(text.value);
 
    }
-   function firstToCapital(){
-    console.log("Lower case was clicked")
-    text.charAt(0).toUpperCase();
-    setText(text.charAt(0).toUpperCase() + text.slice(1));
+   function clearText(){
+   setText("");
    
    }
 
@@ -43,16 +41,16 @@ export default function TextForm(props) {
     <div className="mb-3" >
     <textarea className="form-control" style= {{backgroundColor: props.textArea, color: props.text}} value={text} onChange= {onChangeHandler} id="myBox" rows="8" ></textarea>
      </div >
-     <button className="btn btn-primary mx-2" onClick={toUpperCase}>Convert To Upper Case </button>
-     <button className="btn btn-primary mx-2"onClick={toLowerCase}>Convert To Lower Case</button>
-     <button className="btn btn-primary mx-2"onClick={firstToCapital}>Convert To Sentence Case</button>
-     <button className="btn btn-primary mx-2"onClick={textCopy}>Copy To Clipboard</button>
+     <button className="btn btn-primary mx-2 my-1" onClick={toUpperCase}>Convert To Upper Case </button>
+     <button className="btn btn-primary mx-2 my-1"onClick={toLowerCase}>Convert To Lower Case</button>
+     <button className="btn btn-primary mx-2 my-1"onClick={clearText}>Clear text</button>
+     <button className="btn btn-primary mx-2 my-1"onClick={textCopy}>Copy To Clipboard</button>
 </div>
 
 <div className="container" style= {{color: props.text}} >
     <h1>Text summary:</h1>
     <h6>Number of characters = {text.length}</h6>
-    <h6>Number of words = {text.split(" ").length-1}</h6>
+    <h6>Number of words = {text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</h6>
     <h6>Time to read the whole text: {(0.008 * text.split(" ").length )}</h6>
     <div className="container my-3" style={{border: '.2px solid blue' }}>
     <h3>Preview of the text:</h3>
